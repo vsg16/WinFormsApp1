4fa299f08fc6f72da4fbe3408abd79ff2d86004f
vsg16 <vsg16@tpu.ru>
2023-10-12 15:11:58 +0700

    Добавьте файлы проекта.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            button2 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(23, 98);
+            button1.Name = "button1";
+            button1.Size = new Size(302, 44);
+            button1.TabIndex = 0;
+            button1.Text = "Шифр/Дешифр Полибия";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox1.Location = new Point(185, 47);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.RightToLeft = RightToLeft.No;
+            textBox1.Size = new Size(302, 36);
+            textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Italic, GraphicsUnit.Point);
+            label1.Location = new Point(178, 9);
+            label1.Name = "label1";
+            label1.RightToLeft = RightToLeft.No;
+            label1.Size = new Size(309, 25);
+            label1.TabIndex = 2;
+            label1.Text = "Шифруемое сообщение (исходное)";
+            label1.Click += label1_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox3.Location = new Point(357, 234);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.RightToLeft = RightToLeft.No;
+            textBox3.Size = new Size(302, 39);
+            textBox3.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox2.Location = new Point(23, 234);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.RightToLeft = RightToLeft.No;
+            textBox2.Size = new Size(302, 39);
+            textBox2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(357, 98);
+            button2.Name = "button2";
+            button2.Size = new Size(302, 44);
+            button2.TabIndex = 7;
+            button2.Text = "Шифр/Дешифр ОШБ";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Italic, GraphicsUnit.Point);
+            label2.Location = new Point(224, 160);
+            label2.Name = "label2";
+            label2.RightToLeft = RightToLeft.No;
+            label2.Size = new Size(234, 25);
+            label2.TabIndex = 8;
+            label2.Text = "Результат шифрования:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 14.25F, FontStyle.Italic, GraphicsUnit.Point);
+            label3.Location = new Point(485, 197);
+            label3.Name = "label3";
+            label3.RightToLeft = RightToLeft.No;
+            label3.Size = new Size(58, 25);
+            label3.TabIndex = 9;
+            label3.Text = "ОШБ:";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 14.25F, FontStyle.Italic, GraphicsUnit.Point);
+            label4.Location = new Point(123, 197);
+            label4.Name = "label4";
+            label4.RightToLeft = RightToLeft.No;
+            label4.Size = new Size(93, 25);
+            label4.TabIndex = 10;
+            label4.Text = "Полибий:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 14.25F, FontStyle.Italic, GraphicsUnit.Point);
+            label5.Location = new Point(224, 294);
+            label5.Name = "label5";
+            label5.RightToLeft = RightToLeft.No;
+            label5.Size = new Size(253, 25);
+            label5.TabIndex = 11;
+            label5.Text = "Результат дешифрования:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 14.25F, FontStyle.Italic, GraphicsUnit.Point);
+            label6.Location = new Point(123, 335);
+            label6.Name = "label6";
+            label6.RightToLeft = RightToLeft.No;
+            label6.Size = new Size(93, 25);
+            label6.TabIndex = 15;
+            label6.Text = "Полибий:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 14.25F, FontStyle.Italic, GraphicsUnit.Point);
+            label7.Location = new Point(485, 335);
+            label7.Name = "label7";
+            label7.RightToLeft = RightToLeft.No;
+            label7.Size = new Size(58, 25);
+            label7.TabIndex = 14;
+            label7.Text = "ОШБ:";
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox5.Location = new Point(357, 372);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.RightToLeft = RightToLeft.No;
+            textBox5.Size = new Size(302, 39);
+            textBox5.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox4.Location = new Point(23, 372);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.RightToLeft = RightToLeft.No;
+            textBox4.Size = new Size(302, 39);
+            textBox4.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(673, 423);
+            Controls.Add(label6);
+            Controls.Add(label7);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button2);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            KeyPreview = true;
+            Name = "Form1";
+            Text = "Шифрование алгоритмами: Квадрат Полибия, одноразовый шифровальный блокнот (ОШБ)";
+            Load += Form1_Load;
+            //KeyDown += Form1_KeyDown;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private Button button2;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox textBox5;
+        private TextBox textBox4;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,48 @@
+using System.Data;
+using Class_shifrs;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Polib my = new Polib();
+            string my_polib = "";
+            my_polib = my.coding_eng(textBox1.Text.ToUpper());
+            textBox2.Text = my_polib;
+
+            my_polib = my.decoding_eng(textBox1.Text.ToUpper());
+            textBox4.Text = my_polib;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OSB my_osb = new OSB();
+            textBox3.Text = (my_osb.coding_eng(textBox1.Text.ToUpper())).ToString();
+            string my_osb_new = "";
+            my_osb_new = my_osb.decoding_eng(textBox3.Text);
+            textBox5.Text = my_osb_new;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
